facd111bb830d84bd461c2ea702b40fe9867ac1d
Max <max96132@gmail.com>
2024-08-01 21:30:25 +0300

    Processing line by line.

--- form.js
+++ form.js
@@ -19,11 +19,22 @@ app.post('/process_post', urlencodedParser, function(req, res)
 	var search = req.body.search;
 	console.log(req.body.search);
 	const{exec} = require('child_process');
-	exec(`grep -rl ${search} data`, function(err, stdout, stderr) {
+	exec(`grep -rc ${search} data | sort -n -k2 -t':' -r`, function(err, stdout, stderr) {
 	console.log(stdout);
 	let response = fs.readFileSync("form.html").toString();
-	let out = stdout.replace('\n','<br>')
-	response = response + `<br>Результат по запросу: '${search}':<br>${out}`;
+	//let out = stdout.replace('\n','<br>')
+	let out = stdout.toString().split('\n');
+	let output
+	for (var line of out) {
+    		//console.log(line);
+		[file, n] = line.split(':');
+		console.log(file, n);
+		if ((n > 0) && (file)) {	
+		output = output + file + `(встречается ${n} раз(а))<br>`
+		}
+	}
+	console.log(out);
+	response = response + `<br>Результат по запросу: '${search}':<br>${output}`;
 	res.send(response);
 	})	
 })
